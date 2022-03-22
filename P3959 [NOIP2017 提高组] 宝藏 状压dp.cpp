f9446442785742c390057866db3f0b16e30e2c50
chenliyan090327 <100784819+chenliyan090327@users.noreply.github.com>
2022-03-22 23:04:49 +0800

    Update and rename P3959 [NOIP2017 提高组] 宝藏 动压dp.cpp to P3959 [NOIP2017 提高组] 宝藏 状压dp.cpp

--- P3959 [NOIP2017 提高组] 宝藏 状压dp.cpp
+++ P3959 [NOIP2017 提高组] 宝藏 状压dp.cpp
@@ -25,48 +25,46 @@ int main(){
 	}
 	//printf("n=%d m=%d\n",n,m);
 	//memset(trans,0,sizeof(trans)); 
-    for(int i=0;i<(1<<n);i++){		//±éÀú×´Ì¬
-	    for(int j=i;j!=0;j=(j-1)&i){//×´Ì¬jÎª×´Ì¬i×Ó×´Ì¬£¬
-	        bool OK=true;
-	        int temp=i^j;//×´Ì¬iÓë×´Ì¬jµÄ²»Í¬Ö®´¦£¬×´Ì¬×ªÒÆÎªj->i 
+    for(int i=0;i<(1<<n);i++){		//éå†çŠ¶æ€
+	    for(int j=i;j!=0;j=(j-1)&i){//çŠ¶æ€jä¸ºçŠ¶æ€iå­çŠ¶æ€ï¼Œ
+	       
+	        int temp=i^j;//çŠ¶æ€iä¸çŠ¶æ€jçš„ä¸åŒä¹‹å¤„ï¼ŒçŠ¶æ€è½¬ç§»ä¸ºj->i 
 	        for(int g=n-1;g>=0;g--)   {	     
-	        	if((temp>>g)&1){//ËµÃ÷µãkÊÇ×ªÒÆÖĞÔö¼ÓµÄµã£¬¼´ jÃ»ÓĞ£¬iÓĞ 	            
+	        	if((temp>>g)&1){//è¯´æ˜ç‚¹kæ˜¯è½¬ç§»ä¸­å¢åŠ çš„ç‚¹ï¼Œå³ jæ²¡æœ‰ï¼Œiæœ‰ 	            
 	                int tmin=INF;
 	                for(int L=1;L<=n;L++){
-	                	if(1&(j>>(L-1))){ //Èç¹û×´Ì¬j°üº¬´Ëµã ,Çó³öLµ½g+1µÄ×î¶Ì¾àÀë
+	                	if(1&(j>>(L-1))){ //å¦‚æœçŠ¶æ€jåŒ…å«æ­¤ç‚¹ ,æ±‚å‡ºLåˆ°g+1çš„æœ€çŸ­è·ç¦»
 	                    	//if(((1<<(L-1))&j)==(1<<(L-1))) 
 	                        tmin=min(tmin,road[L][g+1]);
 	                	}
 					} 	 
 					//printf("trans[%d][%d] tmin=%x\n",n,m,tmin);                  	                        
-	                if(tmin==INF){// Èç¹û´ËÂ·ÎŞ·¨×ßÍ¨   ×´Ì¬jµ½×´Ì¬i ²»¿ÉÄÜ   
+	                if(tmin==INF){// å¦‚æœæ­¤è·¯æ— æ³•èµ°é€š   çŠ¶æ€jåˆ°çŠ¶æ€i ä¸å¯èƒ½   
 						trans[j][i]=INF;
 						//printf(" trans[%d][%d]=%x\n",n,m,trans[j][i]);  							                    
 	                    break;
 	                }	            
-	                trans[j][i]+=tmin; // ×´Ì¬jµ½×´Ì¬iµÄ×îĞ¡¿ªÏú  
+	                trans[j][i]+=tmin; // çŠ¶æ€jåˆ°çŠ¶æ€içš„æœ€å°å¼€é”€  
 					//printf("trans[%d][%d]=%x\n",n,m,trans[j][i]);     
 	            }
-			}	
-			if(OK==false)
-                trans[j][i]=0x3f3f3f3f;       
-	    }
+		}	
+			
 	} 
 	memset(f,0x3f,sizeof f);
-    for(int i=1;i<=n;i++)  // ´ÓÒ»²ãÎªÃâ·Ñ 
+    for(int i=1;i<=n;i++)  // ä»ä¸€å±‚ä¸ºå…è´¹ 
         f[1][(1<<(i-1))]=0;
         
-    for(int k=2;k<=n;k++){  // ÓÉµ×ÖÁÉÏDP£¬´Ó2²ã¿ª¼ÆËã 
-    	for(int i=0;i<(1<<n);i++){	//±éÀú×´Ì¬
-	    	for(int j=i;j!=0;j=(j-1)&i){ //×´Ì¬jÎª×´Ì¬i×Ó×´Ì¬£¬
-	    		if(trans[j][i]!=INF){  //Ê¹ÓÃ¼ÆËã 
+    for(int k=2;k<=n;k++){  // ç”±åº•è‡³ä¸ŠDPï¼Œä»2å±‚å¼€è®¡ç®— 
+    	for(int i=0;i<(1<<n);i++){	//éå†çŠ¶æ€
+	    	for(int j=i;j!=0;j=(j-1)&i){ //çŠ¶æ€jä¸ºçŠ¶æ€iå­çŠ¶æ€ï¼Œ
+	    		if(trans[j][i]!=INF){  //ä½¿ç”¨è®¡ç®—è®¡ç®—ç»“æœ 
 	    			f[k][i]=min(f[k][i],f[k-1][j]+(k-1)*trans[j][i]);
 	    		}
 	    	}
 	    }
 	}    
     long long ans=LINF;
-    for(int k=1;k<=n;k++)  // ²éÕÒ²»Í¬²ã¼¶ ËùÓĞÁªÍ¨µÄ×îĞ¡Öµ 
+    for(int k=1;k<=n;k++)  // æŸ¥æ‰¾ä¸åŒå±‚çº§ æ‰€æœ‰è”é€šçš„æœ€å°å€¼ 
         ans=min(ans,f[k][(1<<n)-1]);
 	
 	cout<<ans<<endl;
